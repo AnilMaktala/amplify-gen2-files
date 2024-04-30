14faec5eafa9083e171bb4542c753b33f4a2a7ad
Anil Maktala <amaktala@amazon.com>
2024-04-30 13:22:52 -0400

    init

---
+++ amplify/auth/resource.ts
@@ -0,0 +1,31 @@
+// amplify/auth/resource.ts
+import { defineAuth } from "@aws-amplify/backend";
+
+/**
+ * Define and configure your auth resource
+ * When used alongside data, it is automatically configured as an auth provider for data
+ * @see https://docs.amplify.aws/gen2/build-a-backend/auth
+ */
+export const auth = defineAuth({
+  loginWith: {
+    email: true,
+    // add social providers
+  },
+  /**
+   * Enable multifactor authentication
+   * @see https://docs.amplify.aws/gen2/build-a-backend/auth/manage-mfa
+   */
+  // multifactor: {
+  //   mode: 'OPTIONAL',
+  //   sms: {
+  //     smsMessage: (code) => `Your verification code is ${code}`,
+  //   },
+  // },
+  userAttributes: {
+    /** Request additional attributes for your app's users */
+    // profilePicture: {
+    //   mutable: true,
+    //   required: false,
+    // },
+  },
+});
